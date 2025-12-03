0a7bf4d5cbca3ea8f7aa069451cf38fb5d1cc376
mr-hoffman <hoffmant@hilbertk12.org>
2025-12-03 09:26:34 -0600

    Implement main class and student creation method
    Added a main class with a method to create CollegeStudent objects.

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    
+    // write a while loop to create and display CollegeStudnets using the below method
+    
+  }
+
+  private static CollegeStudent createStudent(Scanner scan) {
+    // complete this method to create a CollegeStudent object with details provided by the user
+    
+  }
+    
+}
